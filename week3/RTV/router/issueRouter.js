4a50bc68861239902f771a1443998cc0a367b6fd
JeromeBlackwell <71746521+JeromeBlackwell@users.noreply.github.com>
2021-08-17 13:24:14 -0400

    week3

--- week3/RTV/router/issueRouter.js
+++ week3/RTV/router/issueRouter.js
@@ -14,74 +14,74 @@ IssueRouter.get('/', (req, res, next) => {
   })
 })
 
-// // Post
-// IssueRouter.post('/', (req, res, next) => {
-//   const newIssue =   new Issue(req.body)
-//   newIssue.save((err, savedIssue) => {
-//       if(err){
-//       res.status(500)
-//       return next(err)
-//     }
-//     return res.status(201).send(savedIssue)
-//   })
-// })
+// Post
+IssueRouter.post('/', (req, res, next) => {
+  const newIssue =   new Issue(req.body)
+  newIssue.save((err, savedIssue) => {
+      if(err){
+      res.status(500)
+      return next(err)
+    }
+    return res.status(201).send(savedIssue)
+  })
+})
 
 
-// //Like comment
-// IssueRouter.put('/like/:IssueRouterID', (req, res, next) => {
-//   Issue.findOneAndUpdate(
-//     { _id: req.params.IssueRouterID },
-//     { $inc: { likes: 1 }},
-//     { new: true },
-//     (err, updatedIssueRouter) => {
-//       if(err) {
-//         res.status(500)
-//         return next(err)
-//       }
-//       return res.status(201).send(updatedIssueRouter)
-//     }
-//   )
-// })
+//Like comment
+IssueRouter.put('/like/:IssueRouterID', (req, res, next) => {
+  Issue.findOneAndUpdate(
+    { _id: req.params.IssueRouterID },
+    { $inc: { likes: 1 }},
+    { new: true },
+    (err, updatedIssueRouter) => {
+      if(err) {
+        res.status(500)
+        return next(err)
+      }
+      return res.status(201).send(updatedIssueRouter)
+    }
+  )
+})
 
-// //Comments by like Range
-// IssueRouter.get('/search/bylikes/:btm/:top', (req, res, next) => {
-//   Issue.where('likes').gte(req.params.btm).lte(req.params.top).exec((err, IssueRouter)=> {
-//     if(err) {
-//       res.status(500)
-//       return next(err)
-//     }
-//     return res.status(200).send(IssueRouter)
-//   })
-// })
+//Comments by like Range
+IssueRouter.get('/search/bylikes/:btm/:top', (req, res, next) => {
+  Issue.where('likes').gte(req.params.btm).lte(req.params.top).exec((err, IssueRouter)=> {
+    if(err) {
+      res.status(500)
+      return next(err)
+    }
+    return res.status(200).send(IssueRouter)
+  })
+})
 
-//   //Update One
-// IssueRouter.put("/:IssueRouterId", (req, res, next) => {
-//   Issue.findOneAndUpdate(
-//     { _id: req.params.IssueRouterId},
-//     req.body,
-//     {new: true},
-//     (err, updatedIssueRouter) => {
-//       if(err){
-//         res.status(500)
-//         return next(err)
-//       }
-//       return res.status(201).send(updatedIssueRouter)
-//     }
-//   )  
-// })
+  //Update One
+IssueRouter.put("/:IssueRouterId", (req, res, next) => {
+  Issue.findOneAndUpdate(
+    { _id: req.params.IssueRouterId},
+    req.body,
+    {new: true},
+    (err, updatedIssueRouter) => {
+      if(err){
+        res.status(500)
+        return next(err)
+      }
+      return res.status(201).send(updatedIssueRouter)
+    }
+  )  
+})
 
-// //Delete One
-// IssueRouter.delete("/:IssueRouterId", (req, res, next) => {
-//   Issue.findOneAndDelete(
-//     {_id: req.params.IssueRouterId}, 
-//     (err, deletedItem) => {
-//       if(err){
-//         res.status(500)
-//         return next(err)
-//       }
-//       return res.status(200).send(`Successfully deleted item ${deletedItem.title} from the database`)
-//     }
-//   )
-// })
+//Delete One
+IssueRouter.delete("/:IssueRouterId", (req, res, next) => {
+  Issue.findOneAndDelete(
+    {_id: req.params.IssueRouterId}, 
+    (err, deletedItem) => {
+      if(err){
+        res.status(500)
+        return next(err)
+      }
+      return res.status(200).send(`Successfully deleted item ${deletedItem.title} from the database`)
+    }
+  )
+})
 
   module.exports = IssueRouter
